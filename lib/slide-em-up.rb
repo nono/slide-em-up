cfa334c41eee7ffa7f935fefc5666a9acabfa0e4
Bruno Michel <bruno.michel@af83.com>
2011-04-19 11:54:44 +0200

    Merge SlidesAPI and AssetsAPI

--- lib/slide-em-up.rb
+++ lib/slide-em-up.rb
@@ -1,5 +1,4 @@
 module SlideEmUp
-  autoload :AssetsAPI,    "slide-em-up/assets_api"
   autoload :Presentation, "slide-em-up/presentation"
   autoload :SlidesAPI,    "slide-em-up/slides_api"
   autoload :VERSION,      "slide-em-up/version"
--- lib/slide-em-up/slides_api.rb
+++ lib/slide-em-up/slides_api.rb
@@ -1,21 +1,59 @@
+require "time"
 require "rack/utils"
+require "rack/mime"
 require "goliath/api"
 
 
 module SlideEmUp
   class SlidesAPI < Goliath::API
-    use ::Rack::ContentLength
-
     def initialize(presentation)
       @presentation = presentation
     end
 
     def response(env)
+      path_info = Rack::Utils.unescape(env["PATH_INFO"])
+      if path_info == "/"
+        serve_slides
+      elsif path_info.include? ".."
+        unauthorized_access
+      else
+        serve_asset(path_info)
+      end
+    end
+
+  protected
+
+    def serve_slides
       body = @presentation.html
       [200, {
         "Content-Type"   => "text/html; charset=utf-8",
         "Content-Length" => Rack::Utils.bytesize(body).to_s
       }, [body] ]
+    end
+
+    def serve_asset(path_info)
+      path = @presentation.path_for_asset(path_info)
+      return page_not_found(path_info) unless path && File.readable?(path)
+      body = File.read(path)
+      [200, {
+        "Last-Modified"  => File.mtime(path).httpdate,
+        "Content-Length" => Rack::Utils.bytesize(body).to_s,
+        "Content-Type"   => Rack::Mime.mime_type(File.extname(path), 'text/plain'),
+      }, [body] ]
+    end
+
+    def page_not_found(path_info)
+      [404, {
+        "Content-Type"   => "text/plain",
+        "Content-Length" => "0"
+      }, ["File not found: #{path_info}\n"] ]
+    end
+
+    def unauthorized_access
+      [403, {
+        "Content-Type"   => "text/plain",
+        "Content-Length" => "0"
+      }, ["Forbidden\n"] ]
     end
   end
 end
